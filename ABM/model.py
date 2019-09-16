6dfb4ffae41ad016f52cbf39da860f15aa41a058
Amir Emami <41763233+amiraliemami@users.noreply.github.com>
2019-09-16 14:15:35 +0100

    2

---
+++ ABM/model.py
@@ -0,0 +1,7 @@
+## Model:
+# Make a y variable.
+# Make a x variable.
+# Change y and x based on random numbers.
+# Make a second set of y and xs, and make these change randomly as well.
+# Work out the distance between the two sets of y and xs.
+
