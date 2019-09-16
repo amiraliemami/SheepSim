00811a4aa45fbfce71183d5cd5a3fbb0db78afdf
Amir Emami <41763233+amiraliemami@users.noreply.github.com>
2019-09-16 14:36:24 +0100

    add abm model code

--- ABM/model.py
+++ ABM/model.py
@@ -5,3 +5,24 @@
 # Make a second set of y and xs, and make these change randomly as well.
 # Work out the distance between the two sets of y and xs.
 
+y0, x0 = 50, 50 # y then x because of reading image files
+print('Initial: ',x0,y0)
+
+from random import choice
+def perturb(x): 
+    pert = choice([-1,1])
+    return x + pert
+
+x0, y0 = perturb(x0), perturb(y0) 
+print('Perturbed: ',x0, y0)
+
+x, y = x0, y0
+for _ in range(5):
+    x, y = perturb(x), perturb(y)
+print('5 more steps... ', x, y)
+
+import numpy as np
+def distance(x0,y0,x1,y1):
+    return np.sqrt((x1-x0)**2 + (y1-y0)**2)
+
+distance(x0,y0,x,y)
