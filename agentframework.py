2d9c36021f69d6c4fa2f26da9be2294eb58724b9
Amir Emami <41763233+amiraliemami@users.noreply.github.com>
2019-09-17 16:59:59 +0100

    check for agent is not self

--- agentframework.py
+++ agentframework.py
@@ -1,27 +1,42 @@
 import random
+import numpy as np
+
+#### FUNCTIONS
 
 def perturb(x): 
     return (x + random.choice([-1,1])) % 100
 
+def distance_between(agents_row_a, agents_row_b):
+    return np.sqrt(
+        ((agents_row_a[0] - agents_row_b[0])**2) 
+        + ((agents_row_a[1] - agents_row_b[1])**2))
+        
+#### AGENT CLASS
+        
 class Agent():
     # protect self.x and y using 'property': https://docs.python.org/3/library/functions.html#property
-    def __init__(self, environment:list):
+    def __init__(self, environment:list,agents:list):
         self.x = random.randint(0,100)
         self.y = random.randint(0,100)
         
         self.environment = environment
         self.store = 0
         
+        self.agents = agents
         
     def set_x(self,x:int):
         self.x = x
     def set_y(self,y:int):
         self.y = y
-    
+    def set_store(self,val:int):
+        self.store = val
+        
     def get_x(self):
         return self.x
     def get_y(self):
         return self.y
+    def get_store(self):
+        return self.store
 
     def move(self):
         self.x, self.y = perturb(self.x), perturb(self.y)
@@ -39,8 +54,18 @@ class Agent():
         if self.store > 100:
             self.environment[self.y][self.x] += 50
             self.store = 50
+    
+    def share_with_neighbours(self,neighbourhood_size): ### EXCLUDE ITSELF!
+        #print(neighbourhood_size)
+        for agent in self.agents:
+            if agent is not self:
+                if distance_between([self.x,self.y],[agent.get_x(),agent.get_y()]) <= 20:
+                    avg = (self.store + agent.get_store())/2
+                    self.store = avg
+                    agent.set_store(avg)
+#            else: # CHECK IF SAME as self 
+#                print('The same!',agent,self)
             
-
 
 
 
