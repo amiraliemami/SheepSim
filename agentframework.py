be1fbe08800562b879586f74cc0831616c77b330
Amir Emami <41763233+amiraliemami@users.noreply.github.com>
2019-09-25 20:53:19 +0100

    fixed indentations

--- agentframework.py
+++ agentframework.py
@@ -10,7 +10,7 @@ import numpy as np
 
 def perturb(x): 
 	"""Given a number, returns a perturbed version of it with equal chance of increase or decrease by 1, mod300.	
-	
+
 	Arguments:
 		x (integer or float): a number
 	Returns:
@@ -20,35 +20,35 @@ def perturb(x):
 
 #### AGENT CLASS
 class Agent():
-		"""Provides the framework for sheep agents and their associated actions.
-		
-		Arguments:
-			environment (matrix): list of lists of numbers corresponding to grass height at each pixel of environment
-			agents (list): list of Agent objects in the simulation
-			init_coords (2-tuple of integers): Determines (x,y) at which this agent is spawned. If None, random x and y are chosen between 0 and 300. (default None)
-			gender (str): 'm' or 'f', the sheep's gender (default: None, causes _gender below to be set randomely).
+	"""Provides the framework for sheep agents and their associated actions.
 
-		Attributes:
-			_x (integer): The sheep's x coordinate, between 0 and 300
-			_y (integer): The sheep's y coordinate, between 0 and 300
-			_gender (str): 'm' or 'f', the sheep's gender (default: random if gender argument is None)
-			_store (integer): Amount of grass eaten and stored by the sheep. Initiates at 0.
-			_pregnancy (integer): Stage of pregnancy the sheep is at. Initiates at 0.
-			_age (integer): Number of runs the sheep has lived for. Initiates at 0.
+	Arguments:
+		environment (matrix): list of lists of numbers corresponding to grass height at each pixel of environment
+		agents (list): list of Agent objects in the simulation
+		init_coords (2-tuple of integers): Determines (x,y) at which this agent is spawned. If None, random x and y are chosen between 0 and 300. (default None)
+		gender (str): 'm' or 'f', the sheep's gender (default: None, causes _gender below to be set randomely).
 
-		Methods:
-			Set methods: 
-				set_x, set_y, set_store, set_pregnancy
-			Get methods: 
-				get_x, get_y, get_store, get_pregnancy, get_gender, get_age
-			Action methods: 
-				move, eat, share_neighbours, mating
-			Other methods:
-				is_dead, increment_age, distance_to
-        """
+	Attributes:
+		_x (integer): The sheep's x coordinate, between 0 and 300
+		_y (integer): The sheep's y coordinate, between 0 and 300
+		_gender (str): 'm' or 'f', the sheep's gender (default: random if gender argument is None)
+		_store (integer): Amount of grass eaten and stored by the sheep. Initiates at 0.
+		_pregnancy (integer): Stage of pregnancy the sheep is at. Initiates at 0.
+		_age (integer): Number of runs the sheep has lived for. Initiates at 0.
+
+	Methods:
+		Set methods: 
+			set_x, set_y, set_store, set_pregnancy
+		Get methods: 
+			get_x, get_y, get_store, get_pregnancy, get_gender, get_age
+		Action methods: 
+			move, eat, share_neighbours, mating
+		Other methods:
+			is_dead, increment_age, distance_to
+	"""
 	
 	def __init__(self, environment:list,agents:list,init_coords=None,gender=None):
-        """Initiates the agent upon creation.
+		"""Initiates the agent upon creation.
 
 		Arguments:
 			environment (list): list of lists of numbers corresponding to grass height at each pixel of environment
@@ -67,138 +67,139 @@ class Agent():
 			agents (list): store given agents as internal attribute
 		"""
 
-        # private attributes
-        if init_coords is None:
-            self._x = random.randint(0,300)
-            self._y = random.randint(0,300)
-        else:
-            self._x = init_coords[0]
-            self._y = init_coords[1]
+		# private attributes
+		if init_coords is None:
+			self._x = random.randint(0,300)
+			self._y = random.randint(0,300)
+		else:
+			self._x = init_coords[0]
+			self._y = init_coords[1]
 
-        if gender is None:
-            self._gender = random.choice(['m','f'])
-        else:
-            self._gender = gender
+		if gender is None:
+			self._gender = random.choice(['m','f'])
+		else:
+			self._gender = gender
 
-        self._store = 0
-        self._pregnancy = 0
-        self._age = 0
+		self._store = 0
+		self._pregnancy = 0
+		self._age = 0
 
-        # public attributes
-        self.environment = environment
-        self.agents = agents
+		# public attributes
+		self.environment = environment
+		self.agents = agents
         
     # functions for accessing private attributes
-    def set_x(self,x:int):
+	def set_x(self,x:int):
 		"""Sets the private _x attribute to given integer."""
-        self._x = x
+		self._x = x
 
-    def set_y(self,y:int):
+	def set_y(self,y:int):
 		"""Sets the private _y attribute to given integer."""
-        self._y = y
+		self._y = y
 
-    def set_store(self,val:int):
+	def set_store(self,val:int):
 		"""Sets the private _store attribute to given integer."""
-        self._store = val
+		self._store = val
 
-    def set_pregnancy(self,val:int):
+	def set_pregnancy(self,val:int):
 		"""Sets the private _pregnancy attribute to given integer."""
-        self._pregnancy = val
-        
-    def get_x(self):
+		self._pregnancy = val
+
+	def get_x(self):
 		"""Returns the private _x attribute"""
-        return self._x
+		return self._x
 
-    def get_y(self):
+	def get_y(self):
 		"""Returns the private _y attribute"""
-        return self._y
+		return self._y
 
-    def get_store(self):
+	def get_store(self):
 		"""Returns the private _store attribute"""
-        return self._store
+		return self._store
 
-    def get_pregnancy(self):
+	def get_pregnancy(self):
 		"""Returns the private _pregnancy attribute"""
-        return self._pregnancy
+		return self._pregnancy
 
-    def get_gender(self): # read-only
+	def get_gender(self): # read-only
 		"""Returns the private _gender attribute. Note: This attribute does not have a set method - it is read-only."""
-        return self._gender
+		return self._gender
 
-    def get_age(self):
+	def get_age(self):
 		"""Returns the private _age attribute"""
-        return self._age
+		return self._age
 
-    # actions
-    def move(self):
+	# actions
+	def move(self):
 		"""Perturbs the agent's x and y coordinates independantly using the perturb() function. Takes no arguments."""
-        self._x, self._y = perturb(self._x), perturb(self._y)
+		self._x, self._y = perturb(self._x), perturb(self._y)
 
-    def eat(self, sick_enabled=False):
+	def eat(self, sick_enabled=False):
 		"""Calling this will cause the sheep to "eat grass" from the coordinate it is standing on in the environment. 
-		
+
 		If the environment has value equal to or more than 10 at the coordinate at which the sheep is currently standing, the sheep will increase its
 		store by 10, and the environment's value here will decrease by 10. If the value here is less than 10, the sheep will add this value to its store
 		and reduce the environment to 0 at this spot. If environment is at 0 at this coordinate, the sheep will not eat.
-		
+
 		Arguments:
 			sick_enabled (bool): if True, sheep sick up 50 onto their current coordinate in the environment if their store reaches 100 (default False)
 		"""
 
-        grass_available = self.environment[self._y][self._x]
+		grass_available = self.environment[self._y][self._x]
 		# eat 10 if grass abundant
-        if grass_available >= 10:
-            self.environment[self._y][self._x] -= 10
-            self._store += 10
+		if grass_available >= 10:
+			self.environment[self._y][self._x] -= 10
+			self._store += 10
 		# do nothing if no grass
-        elif grass_available == 0:
+		elif grass_available == 0:
 			pass
 		# eat what's left if less than 10
 		else:
-            self.environment[self._y][self._x] = 0
-            self._store += grass_available
-        
+			self.environment[self._y][self._x] = 0
+			self._store += grass_available
+
 		if sick_enabled:
-        # sick up 50 of store if store goes above 100
+		# sick up 50 of store if store goes above 100
 			if self._store > 100:
 				self.environment[self._y][self._x] += 50
 				self._store = 50
     
-    def distance_to(self, other):
+	def distance_to(self, other):
 		"""Given another agent, get the Euclidean distance between self and the given agent.
-		
+
 		Arguments:
 			other (Agent class): Other sheep to get distance to.
 		Returns:
 			Euclidean distance (float) to the other sheep.
 		"""
-        return np.sqrt(((self._x - other.get_x())**2) + ((self._y - other.get_y())**2))
+		return np.sqrt(((self._x - other.get_x())**2) + ((self._y - other.get_y())**2))
 
-    def share_with_neighbours(self,neighbourhood_size=20):
+	def share_with_neighbours(self,neighbourhood_size=20):
 		"""Share store with nearby sheep by splitting resources with them.
-		
+
 		Check if any other sheep are within a given radius of self, and if so, share stores by setting the value of the stores for self and
 		the other sheep to the average of the stores between the two.
-		
+
 		Arguments:
 			neighbourhood_size (integer or float): Radius below which sharing is triggered (default 20)
 		"""
-        for agent in self.agents:
+
+		for agent in self.agents:
 			# do not perform action with oneself (not necessary as avg of x and x is x, but is good practice in case we generalise)
-            if agent is not self:
-                if self.distance_to(agent) <= neighbourhood_size:
-                    avg = (self._store + agent.get_store())/2
-                    self._store = avg
-                    agent.set_store(avg)
-           #else: # CHECK IF SAME as self
-           #    print('The same!',agent,self)
+			if agent is not self:
+				if self.distance_to(agent) <= neighbourhood_size:
+					avg = (self._store + agent.get_store())/2
+					self._store = avg
+					agent.set_store(avg)
+			#else: # CHECK IF SAME as self
+			#    print('The same!',agent,self)
+
+######### EXTRAS - Mating, Aging and Dying #################################################################################################
 
 
-    ######### EXTRAS - Mating, Aging and Dying #################################################################################################
-
-    def mating(self, preg_duration=10, min_age=20, min_dist=10, min_store=50):
+	def mating(self, preg_duration=10, min_age=20, min_dist=10, min_store=50):
 		"""Enables mating for the sheep, meaning that female sheep get pregnant if they come close enough to male sheep and give birth to new sheep after a given pegnancy duration.
-		
+
 		Must be run at each iteration of the simulation. If both self and other sheep are of age, have enough food store, are close enough, 
 		and are also of opposite sexes, the female one will get pregnant. Pregnancies progress with each iteration of the simulation and once 
 		the correct duration is reached, a new sheep (new instance of the Agent class) is initiated immediately to the right of the mother. 
@@ -216,8 +217,7 @@ class Agent():
 			Exception: if pregnancy value becomes negative or goes above preg_duration
 		"""
 
-        pregnancy = self._pregnancy
-
+		pregnancy = self._pregnancy
 		if pregnancy == 0: 
 			# if self has pregnancy 0, look for a mate (always true if male, and a necessary condition if female).
 			# proceed if self is of age and has consumed enough food
@@ -230,10 +230,10 @@ class Agent():
 						if self.distance_to(agent) <= min_dist: 
 							# only mate if other sheep is also of min_age and store
 							if (agent.get_age() > min_age) and (agent.get_store() > min_store):
-                            
+
 								# only mate if opposite genders
 								if self._gender == 'f' and agent.get_gender() == 'm':
-								self._pregnancy = 1 
+									self._pregnancy = 1 
 
 								elif self._gender == 'm' and agent.get_gender() == 'f':
 									# only get other sheep pregnant if it's not already
@@ -242,13 +242,13 @@ class Agent():
         
 		elif pregnancy == preg_duration:
 			# Give birth to another sheep to the right if pregnancy duration reached
-            self.agents.append(Agent(self.environment,self.agents,[self._x+1,self._y]))
-            # reset pregnancy after giving birth, ready for the next
+			self.agents.append(Agent(self.environment,self.agents,[self._x+1,self._y]))
+			# reset pregnancy after giving birth, ready for the next
 			self._pregnancy = 0
 		
 		elif pregnancy > 0 and pregnancy < preg_duration:
 			# Advance pregnancy if already pregnant
-            self._pregnancy += 1
+			self._pregnancy += 1
 
 		elif pregnancy > preg_duration:
 			raise Exception("Pregnancy duration exceeded... Something's wrong!")
@@ -256,10 +256,10 @@ class Agent():
 			raise Exception("Negative value for pregnancy duration... Something's wrong!'")
 
 
-    def is_dead(self,max_age=100):
+	def is_dead(self,max_age=100):
 		"""Checks if age has reached the maximum age (default 100) and returns a bool answer."""
-        return (self._age > max_age)
+		return (self._age > max_age)
 
-    def increment_age(self):
+	def increment_age(self):
 		"""Increments the agent's age on which it was called."""
 		self._age += 1
